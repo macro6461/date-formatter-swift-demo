07f67f46877cdc39b8754ea3063aa5166db8fe41
macro6461 <mattcroak718@gmail.com>
2022-05-20 10:48:18 -0400

    Initial Commit

---
+++ dateFormatterDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dateFormatterDemo
+//
+//  Created by Matt C on 5/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dateFormatterDemo/dateFormatterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dateFormatterDemoApp.swift
+//  dateFormatterDemo
+//
+//  Created by Matt C on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct dateFormatterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
